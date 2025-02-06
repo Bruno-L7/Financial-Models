fa4f6a663d161fc21ff7cf4c4e6e9b767cc7a9c0
Bruno-L7 <67074317+Bruno-L7@users.noreply.github.com>
2025-02-05 20:03:56 -0600

    Delete pages/try.py

--- pages/try.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from pypfopt import EfficientFrontier, risk_models, expected_returns
-
-st.title("Test PyPortfolioOpt")
-st.write("If you see this message, the library is imported successfully.")
