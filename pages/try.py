ed116debc09de18e2bccf74f4c28807388c0fb02
Bruno-L7 <67074317+Bruno-L7@users.noreply.github.com>
2025-02-05 19:53:16 -0600

    Create try.py

---
+++ pages/try.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from pypfopt import EfficientFrontier, risk_models, expected_returns
+
+st.title("Test PyPortfolioOpt")
+st.write("If you see this message, the library is imported successfully.")
